74fc552fb23a29afd8647f5fdffef7ca740cd673
baiyeweiguang <baiyeweiguang@163.com>
2024-07-22 19:34:56 +0800

    link eigen3 in rmoss_util/test/CmakeLists.txt

--- rmoss_util/math/test/test_kalman_filter.cpp
+++ rmoss_util/math/test/test_kalman_filter.cpp
@@ -1,4 +1,4 @@
-// Copyright 2023 RoboMaster-OSS
+// Copyright 2024 RoboMaster-OSS
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
